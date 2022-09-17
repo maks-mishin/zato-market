7dd259134ad56d97d18572346fa0bdbbd8ec14e9
maks-mishin <maks.mishinFZ@gmail.com>
2022-09-17 23:54:52 +0300

    feat: add admin models for Category, Product

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(
@@ -14,18 +15,22 @@ class Category(models.Model):
         verbose_name_plural = 'Категории'
 
     def __str__(self) -> str:
-        return self.name
+        return str(self.name)
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products')
+    category = models.ForeignKey(
+        Category,
+        related_name='products',
+        on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
+    description = models.TextField(blank=True, default='')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    avaliable = models.BooleanField(default=True)
+    available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -34,4 +39,4 @@ class Product(models.Model):
         index_together = (('id', 'slug'),)
 
     def __str__(self) -> str:
-        return self.name
+        return str(self.name)
